b25db705fc3cfc8322a7eda3f376a5c41d5c537c
Nikita <7masteryakali@gmail.com>
2023-04-14 10:59:53 +0300

    feat: add count-cats,carbon-dating functions

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let number = 0;
+  matrix.forEach(cat => {
+    cat.forEach(ear=>{
+      if(ear=="^^")
+      ++number;
+    });
+   
+  });
+  return number;
 }
 
 module.exports = {
